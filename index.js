7b6842bfcfed5c4030dba3e937c90e14e237166a
Abdulwadud oluwa <pasmow2@gmail.com>
2022-11-07 22:55:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+function que_1() {
+    let question = document.getElementById('ques');
+    let answer1 = document.getElementById('ans1');
+    let answer2 = document.getElementById('ans2');
+    let answer3 = document.getElementById('ans3');
+    let answer4 = document.getElementById('ans4');
+    answer1.style.display = "block";
+    answer2.style.display = "none";
+    answer3.style.display = "none";
+    answer4.style.display = "none";
+
+}
+function que_2() {
+    let question = document.getElementById('ques');
+    let answer1 = document.getElementById('ans1');
+    let answer2 = document.getElementById('ans2');
+    let answer3 = document.getElementById('ans3');
+    let answer4 = document.getElementById('ans4');
+    answer2.style.display = "block";
+    answer1.style.display = "none";
+    answer3.style.display = "none";
+    answer4.style.display = "none";
+}
+function que_3() {
+    let question = document.getElementById('ques');
+    let answer1 = document.getElementById('ans1');
+    let answer2 = document.getElementById('ans2');
+    let answer3 = document.getElementById('ans3');
+    let answer4 = document.getElementById('ans4');
+    answer3.style.display = "block";
+    answer1.style.display = "none";
+    answer2.style.display = "none";
+    answer4.style.display = "none";
+}
+function que_4() {
+    let question = document.getElementById('ques');
+    let answer1 = document.getElementById('ans1');
+    let answer2 = document.getElementById('ans2');
+    let answer3 = document.getElementById('ans3');
+    let answer4 = document.getElementById('ans4');
+    answer4.style.display = "block";
+    answer1.style.display = "none";
+    answer3.style.display = "none";
+    answer2.style.display = "none";
+}
